629da6cef276f53c07cbc496240db45108473646
addisumotora <addisumotora3@gmail.com>
2024-07-03 21:52:35 +0300

    set default country to Kenya

--- frontend/src/components/kyc-form/KycForm.tsx
+++ frontend/src/components/kyc-form/KycForm.tsx
@@ -3,7 +3,7 @@ import { useVerifyAccountMutation } from "@/redux/api";
 import { useAppSelector } from "@/redux/hooks";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { toast } from "sonner";
 
@@ -18,12 +18,35 @@ function KycForm() {
   const [verifyAccount] = useVerifyAccountMutation();
   const user = useAppSelector(state => state.auth.user)
   const router = useRouter();
+  const [idType, setIdType] = useState("NATIONAL_ID"); 
+  const [sampleImage, setSampleImage] = useState("/assets/img/kyc/id_national.png"); 
   const {
     register,
     handleSubmit,
     setValue,
     formState: { errors },
-  } = useForm<FormValues>();
+  } = useForm<FormValues>({
+    defaultValues: {
+      country: "KE",
+    },
+  });
+
+  useEffect(() => {
+    switch (idType) {
+      case "PASSPORT":
+        setSampleImage("/assets/img/kyc/sample_passport.png");
+        break;
+      case "NATIONAL_ID":
+        setSampleImage("/assets/img/kyc/id_national.png");
+        break;
+      case "ALIEN_CARD":
+        setSampleImage("/assets/img/kyc/alieen_card.png");
+        break;
+      default:
+        setSampleImage("/assets/img/kyc/id_national.png");
+        break;
+    }
+  }, [idType]);
 
   const convertBase64 = (file: File): Promise<string> => {
     return new Promise((resolve, reject) => {
@@ -40,7 +63,6 @@ function KycForm() {
   };
 
   const onSubmit: SubmitHandler<FormValues> = async (data) => {
-    console.log(user, 'user')
     const toastId = toast.loading("please wait...");
     try {
       const file = data.selfie[0];
@@ -52,7 +74,6 @@ function KycForm() {
       });
       router.push("../");
     } catch (error: any) {
-      const errorMessage = "User verification failed, try again";
       toast.error(error?.data.message ?? 'something went wrong', { id: toastId, duration: 1000 });
       console.error("Error:", error);
     }
@@ -100,9 +121,11 @@ function KycForm() {
             </div>
             <select
               id="idType"
-              {...register("idType", { required: "ID Type is required" })}
+              {...register("idType", {
+                required: "ID Type is required",
+                onChange: (e) => setIdType(e.target.value),
+              })}
             >
-              <option value="">Select ID Type</option>
               <option value="PASSPORT">PASSPORT</option>
               <option value="NATIONAL_ID">NATIONAL ID</option>
               <option value="ALIEN_CARD">ALIEN CARD</option>
@@ -128,7 +151,7 @@ function KycForm() {
 
       <div className="sample-image__container">
         <Image
-          src={"/assets/img/kyc/id_national.png"}
+          src={sampleImage}
           width={400}
           height={300}
           alt="kyc-sample-image"
